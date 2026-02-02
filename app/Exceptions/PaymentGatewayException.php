1b3836f4b87bd9be8e7c5eebd9698985f4cf32a6
nensi-ocs <nensi.omeecr@gmail.com>
2026-02-02 12:26:45 +0530

    Stop tracking backend public storage, add to .gitignore

--- app/Exceptions/PaymentGatewayException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Exceptions;
-
-use RuntimeException;
-
-class PaymentGatewayException extends RuntimeException
-{
-}
--- config/demo.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return [
-    'admin_email' => env('DEMO_ADMIN_EMAIL', 'admin@b2b.test'),
-    'brand_name' => env('DEMO_BRAND_NAME', 'Elvee B2B Suite'),
-];
